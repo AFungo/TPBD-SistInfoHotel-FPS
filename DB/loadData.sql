51bece289c46dd6e0b67f6a98a87ab29667bc27b
Santiago Pesce <santi.pesce12@gmail.com>
2022-05-20 00:07:51 -0300

    Triggers de fecha aterior a la corriente, agregue  a tabla  y arregle el formato de las fechas cargadas de

--- DB/loadData.sql
+++ DB/loadData.sql
@@ -58,14 +58,14 @@ insert into `mucama` (`dni_mucama`, `fecha_ingreso`, `sueldo`) values
 
 /*!40000 alter table `gerente` disable keys*/;
 insert into `gerente` (`dni_gerente`, `fecha_ingreso`, `sueldo`) values
-(29934733, '03-02-2020', 97167.00),
-(26013149, '06-10-2011', 127250.00),
-(19452983, '01-01-2010', 150630.00);
+(29934733, '2020-02-03', 97167.00),
+(26013149, '2011-10-06', 127250.00),
+(19452983, '2010-01-01', 150630.00);
 /*!40000 alter table `gerente` enable keys*/;
 
---
---
---
+-- 
+-- Dumping data para la tabla `gestion_hotel_sc.comision`
+-- 
 
 /*!40000 alter table `comision` disable keys*/;
 insert into `comision` (`nro_comision`, `dni_gerente`, `monto`) values
@@ -74,23 +74,23 @@ insert into `comision` (`nro_comision`, `dni_gerente`, `monto`) values
 (124, 19452983, 704.00);
 /*!40000 alter table `comision` enable keys*/;
 
---
---
---
+-- 
+-- Dumping data para la tabla `gestion_hotel_sc.tipo_habitacion`
+-- 
 
 /*!40000 alter table `tipo_habitacion` disable keys*/;
 insert into `tipo_habitacion` (`cod_tipo`, `descripcion`, `costo`) values
-(001,'Es una habitacion economica que cumple con su objetivo.' , 2500),
-(002,'Es una habitacion con ambientes mas espaciosos y una mejor vista.' , 3500),
-(003,'Es una habitacion con algunos lujos extras.' , 4000),
-(004,'Es la habitacion con mejor vista, mejores lujos, mas espaciosa, la mejor.' , 5900);
+(001, 'Es una habitacion economica que cumple con su objetivo.', 2500),
+(002, 'Es una habitacion con ambientes mas espaciosos y una mejor vista.', 3500),
+(003, 'Es una habitacion con algunos lujos extras.', 4000),
+(004, 'Es la habitacion con mejor vista, mejores lujos, mas espaciosa, la mejor.', 5900);
 /*!40000 alter table `tipo_habitacion` enable keys*/;
 
 
 
---
---
---
+-- 
+-- Dumping data para la tabla `gestion_hotel_sc.habitacion`
+-- 
 
 /*!40000 alter table `habitacion` disable keys*/;
 insert into `habitacion` (`nro_habitacion`, `cant_camas`, `cod_tipo`) values
@@ -99,26 +99,26 @@ insert into `habitacion` (`nro_habitacion`, `cant_camas`, `cod_tipo`) values
 (47, 3, 003),
 (5 , 1, 001),
 (32, 1, 001),
-(40, 4, 004);
+(40, 3, 004);
 /*!40000 alter table `habitacion` enable keys*/;
 
---
---
---
+-- 
+-- Dumping data para la tabla `gestion_hotel_sc.atiende`
+-- 
 
 /*!40000 alter table `atiende` disable keys*/;
 insert into `atiende` (`dni_mucama`, `nro_habitacion`) values
-(42788292,23),
-(34148124,18),
-(39102573,47),
-(31228919,5),
-(33578090,32),
-(31228919,40);
+(42788292, 23),
+(34148124, 18),
+(39102573, 47),
+(31228919, 5),
+(33578090, 32),
+(31228919, 40);
 /*!40000 alter table `atiende` enable keys*/;
 
---
---
---
+-- 
+-- Dumping data para la tabla `gestion_hotel_sc.fecha`
+-- 
 
 /*!40000 alter table `fecha` disable keys*/;
 insert into `fecha` (`fecha`) values
@@ -133,22 +133,19 @@ insert into `fecha` (`fecha`) values
 ('2014-09-23');
 /*!40000 alter table `fecha` enable keys*/;
 
---
---
---
+-- 
+-- Dumping data para la tabla `gestion_hotel_sc.ocupada`
+-- 
 
 /*!40000 alter table `ocupada` disable keys*/;
-insert into `ocupada` (`nro_habitacion`,`fecha`, `dni_cliente`,`precio_noche`,`cant_dias`) values
-(23,'2020-05-14', 43189994, 3500,5),
-(18,'2010-11-02', 21437887, 2500,1),
-(47,'2012-01-21', 24150753, 4000,4),
-(5,'2016-08-29', 22943126, 2500,2),
-(32,'2018-06-17', 30214255, 2500,2),
-(23,'2019-07-12', 21437887, 3500,4),
-(47,'2020-01-07', 30214255, 4000,2),
-(18,'2013-09-25', 24150753, 2500,1),
-(40,'2014-09-23', 34148124, 5900,3);
+insert into `ocupada` (`nro_habitacion`, `fecha`, `dni_cliente`, `precio_noche`, `cantid_dias`) values
+(23, '2020-05-14', 43189994, 3500.00, 5),
+(18, '2010-11-02', 21437887, 2500.00, 1),
+(47, '2012-01-21', 24150753, 4000.00, 4),
+(5, '2016-08-29', 22943126, 2500.00, 2),
+(32, '2018-06-17', 30214255, 2500.00, 2),
+(23, '2019-07-12', 21437887, 3500.00, 4),
+(47, '2020-01-07', 30214255, 4000.00, 2),
+(18, '2013-09-25', 24150753, 2500.00, 1),
+(40, '2014-09-23', 34148124, 5900.00, 3);
 /*!40000 alter table `ocupada` enable keys*/;
-
-
-
